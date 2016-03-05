1a321668ce7258ca44f76ab218231f5e9dd8cfea
Vadim Drobinin <vadim@drobinin.com>
2016-03-05 14:00:39 +0100

    Add basic auth methods for the Skyscanner API

---
+++ THackMarch2016/THackMarch2016/TrainsParser.swift
@@ -0,0 +1,13 @@
+//
+//  TrainsParser.swift
+//  THackMarch2016
+//
+//  Created by Vadim Drobinin on 5/3/16.
+//  Copyright © 2016 Alex & Vadim. All rights reserved.
+//
+
+import Foundation
+
+class TrainsParser {
+  
+}
